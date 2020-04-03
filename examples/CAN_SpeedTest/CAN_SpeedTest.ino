7a4def86c2a936a92f48f30fbc4fd016d7370e67
jxltom <jxltom@gmail.com>
2020-04-03 15:54:04 +0800

    Update examples according to updated api

--- examples/CAN_SpeedTest/CAN_SpeedTest.ino
+++ examples/CAN_SpeedTest/CAN_SpeedTest.ino
@@ -1,34 +1,34 @@
 #include <SPI.h>
-#include <mcp2515.h>
- 
+#include "mcp2515.h"
+
 struct can_frame canMsg;
-MCP2515 mcp2515(10);
 int cntr = 0;
 unsigned long oldTime = 0;
- 
- 
-void setup() {
+
+void setup()
+{
   Serial.begin(115200);
-  SPI.begin();
- 
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_125KBPS);
-  mcp2515.setNormalMode();
- 
+
+  mcp2515_init();
+  mcp2515_reset();
+  mcp2515_set_bitrate(CAN_125KBPS);
+  mcp2515_set_normal_mode();
+
   Serial.println("------- CAN Speedtest ----------");
 }
- 
-void loop() {
- 
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+
+void loop()
+{
+  if (mcp2515_read_message(&canMsg) == ERROR_OK)
+  {
     cntr++;
   }
- 
-  if ((millis()-oldTime)>1000) {
+
+  if ((millis() - oldTime) > 1000)
+  {
     oldTime = millis();
     Serial.print(cntr);
     Serial.println(" msg/sec");
-    cntr = 0;      
+    cntr = 0;
   }
- 
 }
--- examples/CAN_write/CAN_write.ino
+++ examples/CAN_write/CAN_write.ino
@@ -1,13 +1,11 @@
 #include <SPI.h>
-#include <mcp2515.h>
+#include "mcp2515.h"
+
 
 struct can_frame canMsg1;
 struct can_frame canMsg2;
-MCP2515 mcp2515(10);
-
 
 void setup() {
-
   canMsg1.can_id  = 0x0F6;
   canMsg1.can_dlc = 8;
   canMsg1.data[0] = 0x8E;
@@ -32,22 +30,20 @@ void setup() {
   
   while (!Serial);
   Serial.begin(115200);
-  SPI.begin();
   
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_125KBPS);
-  mcp2515.setNormalMode();
+  mcp2515_init();
+  mcp2515_reset();
+  mcp2515_set_bitrate(CAN_125KBPS);
+  mcp2515_set_normal_mode();
   
   Serial.println("Example: Write to CAN");
 }
 
 void loop() {
-  
-  mcp2515.sendMessage(&canMsg1);
-  mcp2515.sendMessage(&canMsg2);
+  mcp2515_send_message(&canMsg1);
+  mcp2515_send_message(&canMsg2);
 
   Serial.println("Messages sent");
   
   delay(100);
-
 }
