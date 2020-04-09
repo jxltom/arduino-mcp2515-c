8c625579f75d4d3fe9b753a096d5c58cf9e71aa3
jxltom <jxltom@gmail.com>
2020-04-09 10:53:28 +0800

    Fix bug where IO is not imported from correct header

--- spi_cc2640.h
+++ spi_cc2640.h
@@ -1,7 +1,7 @@
 #ifndef _SPI_CC2640_H_
 #define _SPI_CC2640_H_
 
-#include <ti/drivers/PIN.h>
+#include <ti/devices/cc26x0r2/driverlib/ioc.h>
 
 const uint32_t SPI_CLOCK = 5000000; // 5MHz
 const uint8_t SPICS = IOID_20;
