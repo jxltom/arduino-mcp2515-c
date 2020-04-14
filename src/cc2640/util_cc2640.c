9fe7630767f1477fc5fdeb313ea1d2778fe78942
jxltom <jxltom@gmail.com>
2020-04-14 11:31:41 +0800

    Add implementation of util_millis for cc2640

--- src/cc2640/util_cc2640.c
+++ src/cc2640/util_cc2640.c
@@ -1,12 +1,16 @@
 #include "../config.h"
 #if defined(PLATFORM_CC2640)
 
+#include <ti/sysbios/hal/Seconds.h>
 #include <ti/sysbios/knl/Task.h>
 #include <ti/sysbios/knl/Clock.h>
 #include "../util.h"
 
 unsigned long util_millis() {
-    return 0;
+    Seconds_Time ts;
+    Seconds_getTime(&ts);
+    uint64_t milliseconds = (ts.secs * 1000) + (ts.nsecs / 1000000);
+    return (unsigned long) milliseconds;
 }
 
 void util_delay(unsigned int ms) {
